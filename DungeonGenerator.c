d9e8763edba73f6a5dce0ba4b019b6f090ecb693
David Bone <59885088+davidbone4@users.noreply.github.com>
2021-02-09 21:44:36 -0600

    tried to make an array of structs...cant figure it out

--- DungeonGenerator.c
+++ DungeonGenerator.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int rows = 21;
 int cols = 80;
 
+typedef struct Cells
+{
+    char* type[50];
+    int* hardness;
+} cell;
+
+cell *dungeon[][];
+
+void printCell(struct Cells cell);
+
 int main(int argc, char const *argv[])
 {
 
-    char **dungeon = (char **)malloc(rows * sizeof(char *));
+    typedef struct Cells;
+
+    // cell **dungeon = (cell **)malloc(rows * sizeof(cell *));
+
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     dungeon[i] = (cell *)malloc(cols * sizeof(cell));
+    // }
+
+    struct cell* dungeon[rows][cols];
 
     for (int i = 0; i < rows; i++)
     {
-        dungeon[i] = (char *)malloc(cols * sizeof(char));
+        for (int j = 0; j < cols; j++)
+        {
+            dungeon[i][j] = (cell *)malloc(sizeof(cell));
+            &(dungeon[i][j])->type = (char*) malloc(sizeof(char)*50);
+
+        }
     }
-
-
-
-
-
 
     for (int i = 0; i < cols + 2; i++)
     {
@@ -31,8 +51,9 @@ int main(int argc, char const *argv[])
         printf("|");
         for (int j = 0; j < cols; j++)
         {
-            dungeon[i][j] = '#';
-            printf("%c", dungeon[i][j]);
+
+            dungeon[i][j].type = "ROCK";
+            printCell(dungeon[i][j]);
         }
         printf("|\n");
     }
@@ -45,4 +66,39 @@ int main(int argc, char const *argv[])
     printf("\n");
 
     return 0;
+}
+
+void initaldungeon(struct cell **cell)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cell[i][j].type =
+        }
+    }
+}
+
+void printCell(struct Cells cell)
+{
+    if (strcmp(cell.type, "ROCK"))
+    {
+        printf(" ");
+    }
+    else if (strcmp(cell.type, "ROOM"))
+    {
+        printf(".");
+    }
+    else if (strcmp(cell.type, "CORRIDOR"))
+    {
+        printf("#");
+    }
+    else if (strcmp(cell.type, "UP"))
+    {
+        printf("<");
+    }
+    else if (strcmp(cell.type, "DOWN"))
+    {
+        printf(">");
+    }
 }
